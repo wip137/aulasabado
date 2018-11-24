f0037907d60496fd0a557b254aaa2dd2dbd651b8
wip137 <wendellpitta@gmail.com>
2018-11-24 12:17:56 -0200

    Primeira modificação

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -15,8 +15,8 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Word");
-        // TODO code application logic here
+        System.out.println("ouihipuhpçnpuioiuo");
+        
     }
     
 }
